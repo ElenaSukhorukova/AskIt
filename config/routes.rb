cf45071e47c5bb87242162c6c95dd18124345850
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 12:01:17 +0400

    Add routes of questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/index'
-  root to: "home#index"
+  root to: "pages#index"
+  
+  shallow do
+    resources :users do 
+      resources :questions
+    end
+  end
+
+  resources :questions
+
 end
