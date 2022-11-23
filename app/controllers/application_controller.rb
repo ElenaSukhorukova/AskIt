fd34e0cf892e85a6d8038ec4a9425722ae2d29f9
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-23 17:00:16 +0400

    Add alert flash types in application's controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :info, :danger, :warning, :success, :alert, :notice
+
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
