7398d451470837cc4f2690dfcb8dd533532b5b5f
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-03 13:56:44 +0400

    Add more helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def i18n_model_name(model, count: 1)
+    model.model_name.human(count: count)
+  end
+
+  def attribute_model(model, attr)
+    model.class.human_attribute_name(attr)
+  end
 end
