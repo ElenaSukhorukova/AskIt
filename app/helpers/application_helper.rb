f2978a16fe10cbef5fe7c220bfa44a16527b1d5d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-03 16:04:29 +0400

    Add helpers for titles' view and menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
   def attribute_model(model, attr)
     model.class.human_attribute_name(attr)
   end
+
+  def full_title(page_title = '')
+    base_title = t('page.site_name')
+    page_title ? "#{page_title} | #{base_title}" : "#{base_title}"
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: { current_page: current_page }
+  end
+
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+    css_text = current_page == title ? 'text-secondary' : 'text-white'
+    options[:class] = options[:class] ? (options[:class] + ' ' + css_text) : 
+                                        css_text
+    
+    link_to title, url, options
+  end
 end
