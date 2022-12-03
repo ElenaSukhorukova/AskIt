16e7cd3bd2d74d5225967695242a3a7b268dd148
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-03 13:58:40 +0400

    Add association with answers and validation for username in users' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
   has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 end
