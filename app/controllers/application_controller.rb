33b8786da5d40cb0b497082fd9f67702db95759c
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-03 13:56:00 +0400

    Add errorhanding concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  include ErrorHandling
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :info, :danger, :warning, :success, :alert, :notice
 
-  protected
+  protected  
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
