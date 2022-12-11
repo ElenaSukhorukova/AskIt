f5833c8da66d0b11147940721000a83b8e985643
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-11 13:52:06 +0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+30.times do
+  user_id = [1, 2].sample
+  title = Faker::Hipster.sentence(word_count: 3)
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+  
+  unless Question.find_by(title: title, body: body)
+    Question.create title: title, body: body, user_id: user_id
+  end
+end
+
+60.times do
+  user_id = [1, 2].sample
+  question_id = Question.ids.sample
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+
+  unless Answer.find_by body: body, question_id: question_id
+    Answer.create body: body, user_id: user_id, question_id: question_id
+  end
+end
