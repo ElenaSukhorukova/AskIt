265bd9a855c3c13e381f3b74ac050b102c486c4d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-12 20:02:02 +0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
-30.times do
-  user_id = [1, 2].sample
+(5 - User.count).times do |index|
+  username = [Faker::Address.full_address, ''].sample
+  email = "email#{index+1}@email.com"
+  password = '123Test123!'
+
+  User.create username: username, email: email, password: password
+end
+
+(10 - Question.count).times do
+  user_id = User.ids.sample
   title = Faker::Hipster.sentence(word_count: 3)
   body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
   
@@ -8,12 +16,12 @@
   end
 end
 
-60.times do
-  user_id = [1, 2].sample
-  question_id = Question.ids.sample
-  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+Question.all.each do |question|
+  (10 - Answer.where(question_id: question.id).count).times do
+    user_id = User.ids.sample
+    question_id = question.id
+    body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
 
-  unless Answer.find_by body: body, question_id: question_id
     Answer.create body: body, user_id: user_id, question_id: question_id
   end
 end
