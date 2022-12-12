dd9fac698fadf02d71aaf31cf4a83f2bfa85caf3
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-12 19:55:29 +0400

    Add controllers to registrate and for session

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,61 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  included do 
+    private
+
+    def current_user
+      if session[:user_id].present?
+        @current_user ||= User.find_by(id: session[:user_id]).decorate
+      elsif cookies.encrypted[:user_id].present?
+        user = User.find_by(id: cookies.encrypted[:user_id])
+        if user&.remember_token_authenticated?(cookies.encrypted[:remember_token])
+          sign_in user
+          @current_user ||= user.decorate
+        end
+      end 
+    end
+
+    def remember(user)
+      user.remember_me
+      cookies.encrypted[:remember_token] = { value: user.remember_token, expires: 2.weeks }
+      cookies.encrypted[:user_id] = { value: user.id, expires: 2.weeks }
+    end
+
+    def forget(user)
+      user.forget_me
+      cookies.delete :user_id
+      cookies.delete :remember_token
+    end
+
+    def user_signed_in?
+      current_user.present?
+    end
+
+    def sign_in(user)
+      session[:user_id] = user.id
+    end
+
+    def sign_out
+      forget current_user
+      session.delete :user_id
+      @current_user = nil
+    end
+
+    def require_no_authentication
+      return unless user_signed_in?
+
+      redirect_to root_path,
+        warning: I18n.t('flash.already_signed_in')
+    end
+
+    def require_authentication
+      return if user_signed_in?
+      
+      redirect_to root_path,
+        warning: I18n.t('flash.should_signed_in')
+    end
+    
+    helper_method :current_user, :user_signed_in?
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, except: %i[new create]
+  before_action :set_user!, except: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    render plain: params.to_yaml and return
+    @user = User.new user_params
+    if @user.save
+      sign_in @user
+      redirect_to root_path, 
+        success: I18n.t('flash.registration', username: current_user.name_or_email)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to root_path, 
+        success: I18n.t('flash.update', model: i18n_model_name(@user).downcase)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+   def set_user!
+    @user = User.find params[:id]
+   end
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :old_password)
+    end
+end
