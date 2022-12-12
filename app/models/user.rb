a419821d70ec7bfec6caecb405ce5195455e8b7b
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-12 19:59:20 +0400

    Change the way of registration (dlete devise)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,53 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  attr_accessor :old_password, :remember_token
+  has_secure_password validations: false
+
+  validate :password_presence
+  validates :password, confirmation: true, allow_blank: true
+  validate :password_complexity
+  validate :correct_old_password, on: :update, if: -> { password.present? }
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': { mx: true }
   
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
+
+  def forget_me
+    update_column :remember_token_digest, nil
+    self.remember_token = nil
+  end
+
+  def remember_token_authenticated?(remember_token)
+    return false unless remember_token_digest.present?
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+  end
+
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+
+   
+  private
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def correct_old_password
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+  
+    errors.add :password, 'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presence
+    errors.add(:password, :blank) unless password_digest.present?
+  end
 end
