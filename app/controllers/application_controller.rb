3b83e1333dd30d139b0099e318ef6163006a248a
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-27 17:25:57 +0400

    Add Internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include ErrorHandling
   include ApplicationHelper
   include Authentication
+  include Internationalization
 
   add_flash_types :info, :danger, :warning, :success, :alert, :notice
 end
