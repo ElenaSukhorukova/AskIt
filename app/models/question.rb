7e0de2c651ccb02b54edd7a518f4774598b069f5
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-04 16:56:56 +0400

    Add new models Tag, QuestionTag and add an association Tag-Question :through

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,22 @@
 
 class Question < ApplicationRecord
   include Commentable
-  validates :title, presence: true, length: { minimum: 2 }
-  validates :body, presence: true, length: { minimum: 2 }
 
   belongs_to :user
   has_many :answers, dependent: :destroy
+  has_many :question_tags, dependent: :destroy
+  has_many :tags, through: :question_tags
+
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :body, presence: true, length: { minimum: 2 }
+
+  scope :all_by_tags, lambda { |tags| 
+    questions = includes(:user, :question_tags, :tags)
+    questions = if tags
+                  questions.joins(:tags).where(tags: tags).preload(:tags)
+                else
+                  questions.includes(:question_tags, :tags)
+                end
+    questions.order(created_at: :desc)
+  }
 end
