1dbba80a35f6ea42e396c48c3f9cac532b3b50d4
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-07 13:50:37 +0400

    Add concern of controllers for Pundit

---
+++ app/controllers/concerns/authorization.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Authorization
+  extend ActiveSupport::Concern
+
+  included do
+    include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+
+    def user_not_authorized
+      redirect_to request.referer || root_path,
+                  danger: t('flash.not_authorized')
+    end
+  end
+end
