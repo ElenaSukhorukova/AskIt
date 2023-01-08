8934bcb37a016d8746636c7b84a469a820db3906
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-09 00:15:55 +0400

    Tune mailer

---
+++ app/mailers/password_reset_mailer.rb
@@ -0,0 +1,6 @@
+class PasswordResetMailer < ApplicationMailer
+  def reset_email
+    @user = params[:user]
+    mail to: @user.email, subject: t('password_reset_mailer.reset_email.subject')
+  end
+end
