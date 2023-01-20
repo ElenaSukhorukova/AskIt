c711a67636fdcc439106465d716ace3e3a768b00
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-20 14:40:21 +0400

    Fix a few sintax errors in select.js

--- app/javascript/scripts/select.js
+++ app/javascript/scripts/select.js
@@ -1,16 +1,16 @@
-import TomSelect from 'tom-select/dist/js/tom-select.popular'
-import Translations from './i18n/select.json'
+import TomSelect from 'tom-select/dist/js/tom-select.popular';
+import Translations from './i18n/select.json';
 
-let selects = []
+let selects = [];
 
 document.addEventListener("turbo:before-cache", function() {
   selects.forEach((select) => {
-    select.destroy()
-  })
-})
+    select.destroy();
+  });
+});
 
 const rerender = function() {
-  const i18n = Translations[document.querySelector('body').dataset.lang]
+  const i18n = Translations[document.querySelector('body').dataset.lang];
 
   document.querySelectorAll('select.js-multiple-select').forEach((element) => {
     if(!element.classList.contains('tomselected')) {
@@ -27,29 +27,29 @@ const rerender = function() {
         searchField: 'title',
         create: false,
         load: function(query, callback) {
-          const url = element.dataset.ajaxUrl + '.json?term=' + encodeURIComponent(query)
+          const url = element.dataset.ajaxUrl + '.json?term=' + encodeURIComponent(query);
 
           fetch(url)
             .then(response => response.json())
             .then (json => {
-              callback(json)
+              callback(json);
             }).catch(() => {
-              callback()
-            })
+              callback();
+            });
         },
         render: {
           no_results: function(_data, _escape){
             return '<div class="no-results">' + i18n['no_results'] + '</div>';
           }
         }
-      }
+      };
 
-      const el = new TomSelect(element, opts)
-      selects.push(el)
+      const el = new TomSelect(element, opts);
+      selects.push(el);
     }
-  })
-}
+  });
+};
 
-document.addEventListener("turbo:load", rerender)
-document.addEventListener("turbo:frame-render", rerender)
-document.addEventListener("turbo:render", rerender)
+document.addEventListener("turbo:load", rerender);
+document.addEventListener("turbo:frame-render", rerender);
+document.addEventListener("turbo:render", rerender);
