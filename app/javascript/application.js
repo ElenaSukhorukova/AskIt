8f2767b189b5eb7db4d2fe4699acb9710fb0a244
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-20 14:39:47 +0400

    Add turbo and rails/uhs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,12 @@
 // Entry point for the build script in your package.json
-import * as bootstrap from "bootstrap"
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import * as bootstrap from "bootstrap";
+import { Turbo } from "@hotwired/turbo-rails";
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+Turbo.session.drive = false;
 import "./controllers";
 
-import './scripts/select'
+import './scripts/select';
+
+Rails.start();
+Turbolinks.start();
